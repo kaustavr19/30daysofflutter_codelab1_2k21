4a2a3dd5494df1ccfc426b16e71d9c29ffd3ccb4
kaustavr19 <kaustavr19@users.noreply.github.com>
2021-02-01 00:22:59 +0530

    Create an infinite scrolling ListView step 6 part 2 & 3

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,30 @@ class _RandomWordState extends State<RandomWords> {
   final List<WordPair> _suggestions = <WordPair>[];
   final TextStyle _biggerFont = const TextStyle(fontSize: 18);
   @override
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      itemBuilder: (BuildContext _context, int i) {
+        if (i.isOdd) {
+          return Divider();
+        }
+        final int index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair){
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
   Widget build(BuildContext context) {
     //final wordPair = WordPair.random();
     return MaterialApp(
