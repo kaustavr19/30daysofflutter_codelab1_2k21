6c302009a2aefdb3410419470850edff208741ab
kaustavr19 <kaustavr19@users.noreply.github.com>
2021-01-31 23:49:45 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/kaustavroy19/thirtydaysofflutter_codelab1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kaustavroy19.thirtydaysofflutter_codelab1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: const Center(
+          child: const Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
